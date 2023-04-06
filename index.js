e93c082eadcb9c448edaaa96bc6cb8b4e05397c5
Priyank384 <priyankgupta384@gmail.com>
2023-04-06 22:35:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const fileupload=require('express-fileupload')
+const app = express();
+//middlewares
+app.use(fileupload());
+
+//streaming a vedio--->
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/video', (req, res) => {
+  const filePath = path.join(__dirname, './uploads/video.mp4');
+  const stat = fs.statSync(filePath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize - 1;
+    const chunksize = (end - start) + 1;
+    const file = fs.createReadStream(filePath, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(filePath).pipe(res);
+  }
+});
+
+//Uploading a vedio-->
+app.post('/upload', function(req, res) {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.file;
+    uploadPath = './uploads/' + sampleFile.name;
+  
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+  
+      res.send('File uploaded!');
+    });
+  });
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
